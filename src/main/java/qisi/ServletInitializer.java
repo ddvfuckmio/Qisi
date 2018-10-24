b9d3f608f862a5ffd1a64820e8c2df0c3d581c79
大张杰 <1016653792@qq.com>
2018-10-24 11:24:12 +0800

    spring-boot

---
+++ src/main/java/qisi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package qisi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(QisiApplication.class);
+	}
+
+}
