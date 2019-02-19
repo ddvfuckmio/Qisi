068dd013dfa067910bf4ac7d48014ef7735b17d1
drityddv <1016653792@qq.com>
2019-02-19 12:32:44 +0800

    case

--- src/main/resources/static/js/work/updatePassword.js
+++ src/main/resources/static/js/work/updatePassword.js
@@ -4,120 +4,32 @@ var reset = function () {
     $("#reason").val(null);
 };
 
-var submit = function () {
-    var startDate = $('#startDate').datebox('getValue');
-    var endDate = $('#endDate').datebox('getValue');
-    var reason = $('#reason').val();
-
-    if (startDate === '' || endDate === '' || reason === '') {
-        $.messager.alert('操作有误!', '请填写所有表单信息!');
-        return;
-    }
-
-    $.ajax({
-        url: '/worker/addDayOff',
-        contentType: 'application/json',
-        type: 'post',
-        data: JSON.stringify({
-            startDate: startDate,
-            endDate: endDate,
-            reason: reason,
-        }),
-
-        success: function (data) {
-            console.log(data);
-            if (data.status === 200) {
-                $.messager.alert('操作成功!', '申请记录已经提交!');
-            } else {
-                $.messager.alert('操作失败!', data.msg);
-            }
-        },
-    });
-};
-
-
-//Utils
-function parseDate(dateStr) {
-    var strArray = dateStr.split("-");
-    if (strArray.length == 3) {
-        return new Date(strArray[0], strArray[1] - 1, strArray[2]);
-    } else {
-        return new Date();
-    }
-}
 
 $(function () {
 
     //请假窗口
-    $('#div-addDayOff').dialog({
-        title: '员工请假',
-        width: 300,
-        height: 300,
+    $('#div-dialog').dialog({
+        title: '修改密码',
+        width: 250,
+        height: 250,
         modal: true,
         iconCls: 'icon-login',
         onClose: function () {
             $(this).dialog('destroy');          //销毁
-            $('#tabs').tabs('close', '新增请假') //关闭标签页
+            $('#tabs').tabs('close', '修改密码') //关闭标签页
         },
-
     });
 
-    $('#startDate').datebox({
-        width: 180,
+
+    $('#password').validatebox({
         required: true,
-        editable: false,
-        onSelect: function (date) {
-            var endDate = $('#endDate').datebox('getValue');
-            if (endDate != null || endDate != '') {
-                var end = parseDate(endDate);
-                if (date < end) {
-                    $.messager.alert('操作有误!', '当前时间矛盾,已重置结束时间!');
-                    $('#endDate').datebox('setValue', null);
-                    return;
-                }
-            }
-        },
+        missingMessage: '登录密码不能为空!',
     });
 
-    $('#endDate').datebox({
-        width: 180,
+    $('#newPassword').validatebox({
         required: true,
-        editable: false,
-        onSelect: function (date) {
-            var startDate = $('#startDate').datebox('getValue');
-            if (startDate == '') {
-                $.messager.alert('操作失败!', '请先选择开始日期!');
-                $('#endDate').datebox('setValue', null);
-                return;
-            }
-            var start = parseDate(startDate);
-            if (date < start) {
-                $.messager.alert('操作失败!', '结束日期必须大于开始或者等于日期!');
-                $('#endDate').datebox('setValue', null);
-                return;
-            }
-        },
+        missingMessage: '新的密码不能为空!',
     });
-
-
-    $('#startDate').datebox('calendar').calendar({
-        firstDay: 1,
-        validator: function (date) {
-            var now = new Date();
-            var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-            return date >= today;
-        },
-    });
-
-    $('#endDate').datebox('calendar').calendar({
-        firstDay: 1,
-        validator: function (date) {
-            var now = new Date();
-            var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-            return date >= today;
-        },
-    })
 
     $.parser.parse();
-})
-;
+});
